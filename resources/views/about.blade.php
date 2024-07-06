bdfb40c8c40208f82b21c7ee1f1a3c7288621d96
ntl1997 <luandajk4@gmail.com>
2024-07-06 17:20:30 +0700

    init app

---
+++ resources/views/about.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+@empty(!$name)
+    <h3>Name is not empty</h3>
+@endempty
